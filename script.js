1847f72931ea0b8c0f5588381dd65f3bd468a3b8
ManDaMan002 <77306080+ManDaMan002@users.noreply.github.com>
2022-02-17 09:19:33 -0800

    Bug Fixes & UI changes
    Changed UI color (orangered -> #aaaa00)
    Fixed word counter stuck at 1, and fixed regex character matching

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const wordsList = ["Abandoned Mansion", "Abonded Mansion", "Advent Calendar", "Air Horn", "Airplane", "Airport", "Airship",
+// prettier-ignore
+var wordsList = ["Abandoned Mansion", "Abonded Mansion", "Advent Calendar", "Air Horn", "Airplane", "Airport", "Airship",
              "Aladdin", "Alarm", "Alarm Clock", "Alien", "Ambulance", "America", "American Football", "Anchor", "Angel",
              "Angry", "Animal", "Animals", "Ant", "Anvil", "Ape", "Apocalypse", "Apple", "Apple Pie", "Apple Tree",
              "Aquarium", "Arcade", "Arcade Game", "Arcade Machine", "Archer", "Archery", "Arm", "Armor", "Army",
@@ -213,55 +214,70 @@ const wordsList = ["Abandoned Mansion", "Abonded Mansion", "Advent Calendar", "A
 
 function findMatches(word) {
     let result = [];
-    word = word.toLowerCase().replace(/_/g, ".")
+    word = word.toLowerCase().replace(/_/g, "\\w").replace(/\s/g, "\\s");
     for (let i = 0; i < wordsList.length; i++) {
-        let currentWord = wordsList[i].toLowerCase()
-        let match = currentWord.match(`^${word}$`)
+        let currentWord = wordsList[i].toLowerCase();
+        let match = currentWord.match(`^${word}$`);
         if (match) {
-            result.push(match[0])
+            // console.log(match);
+            result.push(match[0].replace(/\\s/g, " "));
         }
-    }    
+    }
     return result;
 }
 
 function exit() {
-    helpCard.style = 'opacity: 0';
-    helpBackground.style = 'opacity: 0; z-index: 0';
-    body.style = 'overflow: auto';
+    helpCard.style = "opacity: 0";
+    helpBackground.style = "opacity: 0; z-index: 0";
+    body.style = "overflow: auto";
 }
 
-const input = document.getElementById("main")
-const helpButton = document.getElementById("HelpButton")
+function updateMatches(key) {
+    if (key) if (key.code == "ShiftLeft" || key.code == "ShiftRight") return;
+    let matches = findMatches(input.value);
+    ul.innerHTML = "";
+    characterCount.innerHTML = `${input.value.length} Character${
+        input.value.length == 1 ? "" : "s"
+    }, ${input.value.length != 0 ? input.value.split(" ").length : 0} Word${
+        input.value.split(" ").length == 1 && input.value.length != 0 ? "" : "s"
+    }, ${findMatches(input.value).length} Match${
+        findMatches(input.value).length != 1 ? "es" : ""
+    }`;
+    for (let i = 0; i < matches.length; i++) {
+        let child = document.createElement("li");
+        child.role = "button";
+        child.innerHTML = matches[i];
+        child.onclick = () => {
+            navigator.clipboard.writeText(matches[i]);
+        };
+        ul.appendChild(child);
+    }
+}
+
+const input = document.getElementById("main");
+const helpButton = document.getElementById("HelpButton");
 const ul = document.getElementsByTagName("ul")[0];
 const characterCount = document.getElementById("character-count");
-const helpCard = document.getElementById('helpcard');
+const helpCard = document.getElementById("helpcard");
 const helpBackground = document.getElementsByClassName("helpbg")[0];
-const exitButton = document.getElementById('x');
+const exitButton = document.getElementById("x");
+const clearButton = document.getElementsByClassName("x")[0];
 const container = document.getElementsByClassName("container")[0];
 const body = document.getElementsByTagName("body")[0];
 
-input.addEventListener("keyup", key => {
-    if (key.code == "ShiftLeft" || key.code == "ShiftRight")
-        return;
-    let matches = findMatches(input.value);
-    ul.innerHTML = '';
-    characterCount.innerHTML = `${input.value.length} Character${input.value.length == 1 ? "" : "s"}, ${input.value.split(" ").length} Word${input.value.split(" ").length == 1 ? "" : "s"}, ${findMatches(input.value).length} Match${findMatches(input.value).length != 1 ? "es" : ""}`;
-    for (let i = 0; i < matches.length; i++) {
-        let child = document.createElement("li")
-        child.role = 'button'
-        child.innerHTML = matches[i]
-        child.onclick = () => {
-            navigator.clipboard.writeText(matches[i])
-        }
-        ul.appendChild(child)
-    }
-})
+input.addEventListener("keyup", (key) => {
+    updateMatches(key);
+});
 
 helpButton.addEventListener("click", () => {
-    helpBackground.style = 'opacity: 1; z-index: 6';
-    helpCard.style = 'opacity: 1; z-index: 7';
-    container.style = 'z-index';
-    body.style = 'overflow: hidden';
-})
+    helpBackground.style = "opacity: 1; z-index: 6";
+    helpCard.style = "opacity: 1; z-index: 7";
+    container.style = "z-index";
+    body.style = "overflow: hidden";
+});
 
-exitButton.addEventListener("click", exit);
+helpBackground.addEventListener("click", exit);
+clearButton.addEventListener("click", () => {
+    input.value = "";
+    updateMatches();
+});
